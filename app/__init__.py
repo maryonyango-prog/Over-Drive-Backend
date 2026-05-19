6737140ff55defc735c111c56a9bbb6870e8d2ab
JealousYoungMaster <nelsonromeook@gmail.com>
2026-05-20 02:41:13 +0300

    Added migrations to the backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -9,6 +11,7 @@ def create_app():
     app.config.from_object("app.config.Config")
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from app.routes.vehicle_routes import vehicle_bp
     from app.routes.media_routes import media_bp
@@ -17,8 +20,5 @@ def create_app():
     app.register_blueprint(vehicle_bp)
     app.register_blueprint(media_bp)
     app.register_blueprint(auth_bp)
-
-    with app.app_context():
-        db.create_all()
 
     return app
